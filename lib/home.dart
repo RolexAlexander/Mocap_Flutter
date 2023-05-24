cde9df01b0023836af6bd7678efacff3c2b86636
Rolex Alexander <rolexalexander67@gmail.com>
2023-05-24 17:50:40 -0400

    dsffdf

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import "package:flutter_application_3/main.dart";
 import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:mime/mime.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -17,16 +20,18 @@ class _HomeState extends State<Home> {
   CameraImage? cameraImage;
   CameraController? cameracontroller;
   String output = "";
+  File? _selectedImage;
   var requestresponse;
   var count = 0;
+  var selectedimage;
+  String? _base64Image;
+  String imagedata = "";
 
   @override
   void initState() {
     super.initState();
     loadCamera();
     loadmodel();
-    // sendPostRequest();
-    print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
   }
 
   loadCamera() {
@@ -71,72 +76,44 @@ class _HomeState extends State<Home> {
     }
   }
 
-  // sendPostRequest() async {
-  //   print(
-  //       "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
-  //   // if (cameraImage != null) {
-  //   print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-  //   // List imageBytes = cameraImage!.planes.map((plane) {
-  //   //   return plane.bytes;
-  //   // }).toList();
-
-  //   // // Convert bytes to base64
-  //   // List<String> base64Images = imageBytes.map((bytes) {
-  //   //   return base64Encode(bytes);
-  //   // }).toList();
-  //   var headers = {
-  //     'Authorization':
-  //         "Token 681abcae51894ac84558c653954f1d887ba6f3e9dd5b0395c44ddd178deea3ef",
-  //     'Content-Type': 'application/json'
-  //   };
-  //   var url = Uri.parse(
-  //       'https://e00f-190-93-37-193.ngrok-free.app/js_public/walker_callback/82cdbffa-bb03-42b6-a553-b775961eabc3/3280bb4b-22e9-4973-ba75-0f1ba6e18404?key=3a7fdc0069733f5e12e16f668f5da103');
-  //   var body = jsonEncode({
-  //     "name": "talker",
-  //     "ctx": {
-  //       "image_data": {"image": "base64Images"}
-  //     },
-  //     "_req_ctx": {},
-  //     "snt": "urn:uuid:fc4bdf0f-ccb6-4f86-bdb6-1787f379fdf5"
-  //   });
-  //   var response = await http.post(url, body: body, headers: headers);
-  //   print(
-  //       'Request xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx response: ${response}');
-  //   setState(() {
-  //     requestresponse = response;
-  //   });
-  //   // }
-  // }
-
   sendPostRequest() async {
-    print(
-        "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
-    // if (cameraImage != null) {
-    print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-    // List imageBytes = cameraImage!.planes.map((plane) {
-    //   return plane.bytes;
-    // }).toList();
+    // Convert bytes to base64
 
-    // Convert bytes to base64
-    // String base64Images = base64Encode(imageBytes);
-    List<List<int>> imageBytes = cameraImage!.planes.map((plane) {
-      return plane.bytes.toList();
-    }).toList();
-    // Convert bytes to base64
-    String base64Images = base64.encode(concatenateByteArrays(imageBytes));
-    print(base64Images.length);
+    try {
+      // Capture an image using the camera controller
+      final image = await cameracontroller?.takePicture();
+
+      setState(() {
+        // Store the captured image file
+        _selectedImage = File(image!.path);
+      });
+    } catch (e) {
+      print('Error occurred: $e');
+    }
+    if (_selectedImage != null) {
+      // Read the image file as bytes
+      final bytes = await _selectedImage!.readAsBytes();
+      print("bytes");
+      print(bytes);
+      // Encode the image bytes to base64
+      final base64Image = base64Encode(bytes);
+
+      setState(() {
+        // Store the base64-encoded image string
+        _base64Image = base64Image;
+      });
+    }
+
     var headers = {
       'Authorization':
-          "Token 6f20aa57cc2ada063004e8b13737c398a69cd20b5fb0b856eebd033229474833",
+          "Token 392f1eb645b5695c7932aec4ef6b1c1f8c00d77f3879829acf38eef20458e879",
       'Content-Type': 'application/json'
     };
     var url = Uri.parse(
-        'https://a56e-190-93-37-193.ngrok-free.app/js_public/walker_callback/82cdbffa-bb03-42b6-a553-b775961eabc3/d33d678c-799c-4f14-b8c3-d3281afce776?key=3a7fdc0069733f5e12e16f668f5da103');
+        'https://5ed4-190-93-37-91.ngrok-free.app/js_public/walker_callback/82cdbffa-bb03-42b6-a553-b775961eabc3/fa709d99-9366-49ca-8b77-f6ffeaefea17?key=3a7fdc0069733f5e12e16f668f5da103');
     var body = jsonEncode({
-      "name": "talker",
-      "ctx": {
-        "image_data": {"image": base64Images}
-      },
+      "name": "interact",
+      "ctx": {"image_data": _base64Image},
       "_req_ctx": {},
       "snt": "urn:uuid:fc4bdf0f-ccb6-4f86-bdb6-1787f379fdf5"
     });
@@ -150,14 +127,6 @@ class _HomeState extends State<Home> {
   loadmodel() async {
     await Tflite.loadModel(
         model: "assets/model.tflite", labels: "assets/labels.txt");
-  }
-
-  List<int> concatenateByteArrays(List<List<int>> byteArrays) {
-    List<int> result = [];
-    for (var byteArray in byteArrays) {
-      result.addAll(byteArray);
-    }
-    return result;
   }
 
   @override
