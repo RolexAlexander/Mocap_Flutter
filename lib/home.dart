e0f678ee197aa8498cec30cf0750be564693e29b
Rolex Alexander <rolexalexander67@gmail.com>
2023-05-29 16:23:24 -0400

    Combined Code

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import "package:flutter_application_3/main.dart";
 import 'package:flutter_tflite/flutter_tflite.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:mime/mime.dart';
+import 'dart:async';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,6 +21,15 @@ class _HomeState extends State<Home> {
   CameraImage? cameraImage;
   CameraController? cameracontroller;
   String output = "";
+  late File _selectedImage;
+  Timer? timer;
+  var requestresponse;
+  var count = 0;
+  var selectedimage;
+  String _base64Image = "";
+  String imagedata = "";
+  bool verified = false;
+  bool newuser = false;
 
   @override
   void initState() {
@@ -30,8 +46,12 @@ class _HomeState extends State<Home> {
       } else {
         setState(() {
           cameracontroller!.startImageStream((imageStream) {
-            cameraImage = imageStream;
-            runModel();
+            count++;
+            if (count % 100 == 0) {
+              print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzxx");
+              cameraImage = imageStream;
+              runModel();
+            }
           });
         });
       }
@@ -56,6 +76,87 @@ class _HomeState extends State<Home> {
           output = element["label"];
         });
       });
+      try {
+        if (predictions[0]["label"] == "2 none") {
+          print("No user infront of camera");
+          verified = false;
+          newuser = false;
+        } else {
+          newuser = true;
+          if (newuser == true && verified == true) {
+            print("User already verified");
+          } else {
+            verified = true;
+            print("new user we have to verify");
+            // send post request to backend
+            _getImageFromCamera();
+          }
+        }
+      } catch (e) {
+        print("Uknown Error");
+      }
+    }
+  }
+
+  void _getImageFromCamera() {
+    print("zzzzzzzzzzzzzzzzsssssss");
+    cameracontroller!.initialize().then((_) {
+      print("zzzzzzzzzzzzzzzzpppppppp");
+      // Capture an image using the camera controller
+      cameracontroller!.takePicture().then((image) {
+        print("zzzzzzzzzzzzzzzzttttttt");
+        setState(() {
+          // Store the captured image file
+          _selectedImage = File(image.path);
+          _convertToBase64();
+        });
+      }).catchError((error) {
+        print('Error occurred: $error');
+      });
+    });
+  }
+
+  void _convertToBase64() {
+    // if (_selectedImage != null) {
+    // Read the image file as bytes
+    _selectedImage.readAsBytes().then((bytes) {
+      // Encode the image bytes to base64
+      final base64Image = base64Encode(bytes);
+
+      setState(() {
+        // Store the base64-encoded image string
+        _base64Image = base64Image;
+      });
+
+      // Send the POST request with the base64 image
+      sendPostRequest();
+    }).catchError((error) {
+      print('Error occurred: $error');
+    });
+    // }
+  }
+
+  Future<void> sendPostRequest() async {
+    final url =
+        'https://0ece-190-93-37-191.ngrok-free.app/js_public/walker_callback/82cdbffa-bb03-42b6-a553-b775961eabc3/ca639c31-e3f3-4a1e-a6ad-ebc4da6c82cd?key=3a7fdc0069733f5e12e16f668f5da103';
+    final headers = {
+      'Authorization':
+          'token 2ef828b2935f311fdec9d6b1bed469e467dbf6b2b7538b63ee8f5320c8a47848',
+      'Content-Type': 'application/json'
+    };
+    final body = jsonEncode({
+      'name': 'interact',
+      'ctx': {'image_data': _base64Image},
+      '_req_ctx': {},
+      'snt': 'urn:uuid:fc4bdf0f-ccb6-4f86-bdb6-1787f379fdf5'
+    });
+
+    try {
+      final response =
+          await http.post(Uri.parse(url), headers: headers, body: body);
+      print('Response: ${response.body}');
+    } catch (error) {
+      print('Error: $error');
     }
   }
 
