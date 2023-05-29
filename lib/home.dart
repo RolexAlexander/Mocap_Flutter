f4acf48a9ea7195c3784bc97a52f564e4afd56b8
Rolex Alexander <rolexalexander67@gmail.com>
2023-05-29 10:39:27 -0400

    Working base64

--- lib/home.dart
+++ lib/home.dart
@@ -20,11 +20,11 @@ class _HomeState extends State<Home> {
   CameraImage? cameraImage;
   CameraController? cameracontroller;
   String output = "";
-  File? _selectedImage;
+  late File _selectedImage;
   var requestresponse;
   var count = 0;
   var selectedimage;
-  String? _base64Image;
+  String _base64Image = "";
   String imagedata = "";
 
   @override
@@ -71,51 +71,65 @@ class _HomeState extends State<Home> {
       // });
       count = count + 1;
       if (count % 10 == 0) {
-        sendPostRequest();
+        _getImageFromCamera();
+        // sendPostRequest();
       }
+    }
+  }
+
+  void _getImageFromCamera() {
+    print("zzzzzzzzzzzzzzzzsssssss");
+    cameracontroller!.initialize().then((_) {
+      print("zzzzzzzzzzzzzzzzpppppppp");
+      // Capture an image using the camera controller
+      cameracontroller!.takePicture().then((image) {
+        print("zzzzzzzzzzzzzzzzttttttt");
+        setState(() {
+          // Store the captured image file
+          _selectedImage = File(image.path);
+          _convertToBase64();
+        });
+      }).catchError((error) {
+        print('Error occurred: $error');
+      });
+    });
+  }
+
+  void _convertToBase64() {
+    if (_selectedImage != null) {
+      // Read the image file as bytes
+      _selectedImage!.readAsBytes().then((bytes) {
+        // Encode the image bytes to base64
+        final base64Image = base64Encode(bytes);
+
+        setState(() {
+          // Store the base64-encoded image string
+          _base64Image = base64Image;
+        });
+
+        // Send the POST request with the base64 image
+        sendPostRequest();
+      }).catchError((error) {
+        print('Error occurred: $error');
+      });
     }
   }
 
   sendPostRequest() async {
     // Convert bytes to base64
 
-    try {
-      // Capture an image using the camera controller
-      final image = await cameracontroller?.takePicture();
-
-      setState(() {
-        // Store the captured image file
-        _selectedImage = File(image!.path);
-      });
-    } catch (e) {
-      print('Error occurred: $e');
-    }
-    if (_selectedImage != null) {
-      // Read the image file as bytes
-      final bytes = await _selectedImage!.readAsBytes();
-      print("bytes");
-      print(bytes);
-      // Encode the image bytes to base64
-      final base64Image = base64Encode(bytes);
-
-      setState(() {
-        // Store the base64-encoded image string
-        _base64Image = base64Image;
-      });
-    }
-
     var headers = {
       'Authorization':
-          "Token 392f1eb645b5695c7932aec4ef6b1c1f8c00d77f3879829acf38eef20458e879",
+          "Token 2ef828b2935f311fdec9d6b1bed469e467dbf6b2b7538b63ee8f5320c8a47848",
       'Content-Type': 'application/json'
     };
     var url = Uri.parse(
-        'https://5ed4-190-93-37-91.ngrok-free.app/js_public/walker_callback/82cdbffa-bb03-42b6-a553-b775961eabc3/fa709d99-9366-49ca-8b77-f6ffeaefea17?key=3a7fdc0069733f5e12e16f668f5da103');
+        'https://0ece-190-93-37-191.ngrok-free.app/js_public/walker_callback/82cdbffa-bb03-42b6-a553-b775961eabc3/ca639c31-e3f3-4a1e-a6ad-ebc4da6c82cd?key=3a7fdc0069733f5e12e16f668f5da103');
     var body = jsonEncode({
       "name": "interact",
       "ctx": {"image_data": _base64Image},
       "_req_ctx": {},
-      "snt": "urn:uuid:fc4bdf0f-ccb6-4f86-bdb6-1787f379fdf5"
+      "snt": "urn:uuid:82cdbffa-bb03-42b6-a553-b775961eabc3"
     });
     var response = await http.post(url, body: body, headers: headers);
     print(
